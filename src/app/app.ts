75c66bf20ab4c37fcd8011d7a44e392c24ad0f77
ruanalexandreS <ruancampos366@gmail.com>
2026-02-26 00:10:09 -0300

    chore: ativa vercel analytics para monitorar tráfego

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 @Component({
   selector: 'app-root',
