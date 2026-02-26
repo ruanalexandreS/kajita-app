afb0e647b80f6b2b322af4bdda63ae68465ae05c
ruanalexandreS <ruancampos366@gmail.com>
2026-02-26 00:23:09 -0300

    fix: resolve erro de build adicionando dependências e checagem de plataforma

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,19 +4,26 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { inject as injectAnalytics } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
-styleUrls: ['./app.scss'] as any
+  styleUrls: ['./app.scss'] as any
 })
-export class App implements OnInit{
+export class App implements OnInit {
   protected readonly title = signal('KajitA');
   ngOnInit(): void {
     // Inicializa a monitorização de tráfego assim que o site carrega
     injectAnalytics();
     injectSpeedInsights();
-}
+    const platformId = inject(PLATFORM_ID);
+    if (isPlatformBrowser(platformId)) {
+      injectAnalytics();
+      injectSpeedInsights();
+    }
+  }
 }
